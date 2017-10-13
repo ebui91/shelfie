bbf935a6d2dd05f3d29a1b5214c85e1f87eecceb
ericbui2@gmail.com <ebui91@Erics-MacBook-Pro.local>
2017-10-13 12:28:30 -0500

    switched from OpenWeatherMap to DarkSky API. Got Skycons working

--- weather5.js
+++ weather5.js
@@ -9,13 +9,12 @@ $(document).ready(function(){
     var city=geoData.city;
     var region=geoData.regionName;
     var country=geoData.country;
-    var api="http://api.openweathermap.org/data/2.5/forecast?lat="+latitude+"&lon="+longitude+"&appid=7d768f2db1dc9554926e225b6ff8e222";
-
+    var api="https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/e9b163b3a73f3035976f90ae46fe58cf/"+latitude+","+longitude;
     $("#location").html(city+", "+region+", "+country);
 
   $.getJSON(api, function(data){
 //Conversion from UNIX timestamp to actual date
-  var timestamp=data.list[0].dt;
+  var timestamp=data.daily.data[0].time;
   var a=new Date(timestamp*1000);
   var days=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
   var dayOfWeek=days[a.getDay()]
@@ -26,17 +25,20 @@ $(document).ready(function(){
   $("#day5").html(days[(a.getDay())+4]);
 
 //Weather Icons
-  $("#icon1").html("<img src='http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
-  $("#icon2").html("<img src='http://openweathermap.org/img/w/" + data.list[3].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
-  $("#icon3").html("<img src='http://openweathermap.org/img/w/" + data.list[6].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
-  $("#icon4").html("<img src='http://openweathermap.org/img/w/" + data.list[9].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
-  $("#icon5").html("<img src='http://openweathermap.org/img/w/" + data.list[12].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+  var skycons = new Skycons({"color": "white", "resizeClear": true});
+  skycons.add(document.getElementById("icon1"), data.currently.icon);
+  skycons.add(document.getElementById("icon2"), data.daily.data[1].icon);
+  skycons.add(document.getElementById("icon3"), data.daily.data[2].icon);
+  skycons.add(document.getElementById("icon4"), data.daily.data[3].icon);
+  skycons.add(document.getElementById("icon5"), data.daily.data[4].icon);
+  skycons.play();
 
 //Temperatures
   //var cTemp=((data.main.temp)-273.15).toFixed(2);
   //var fTemp=((data.main.temp)*(9/5)-459.67).toFixed(2);
-  var temp1=data.list[0].main.temp;
-  var temp2=data.list[1].main.temp;
+  var temp1=data.currently.temperature;
+  $("#temp1").html(temp1+"&#8457");
+  /*var temp2=data.list[1].main.temp;
   var temp3=data.list[2].main.temp;
   var temp4=data.list[3].main.temp;
   var temp5=data.list[4].main.temp;
@@ -56,7 +58,7 @@ $(document).ready(function(){
   $("#cond2").html(cond2.toUpperCase());
   $("#cond3").html(cond3.toUpperCase());
   $("#cond4").html(cond4.toUpperCase());
-  $("#cond5").html(cond5.toUpperCase());
+  $("#cond5").html(cond5.toUpperCase());*/
   });
 
   });
